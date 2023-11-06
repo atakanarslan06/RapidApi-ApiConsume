6b1275816c83047a6edc2019be5df0fcd02d0c9c
Atakan Arslan <atak.arslan@hotmail.com>
2023-11-06 19:09:20 +0300

    service

---
+++ Frontend/HotelProject.WebUI/Dtos/ServiceDto/CreateServiceDto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace HotelProject.WebUI.Dtos.ServiceDto
+{
+    public class CreateServiceDto
+    {
+        [Required(ErrorMessage = "Servis ikon linki giriniz")]   
+        public string ServiceIcon { get; set; }
+
+        [Required(ErrorMessage = "Hizmet başlığı giriniz")]
+        public string Title { get; set; }
+
+        
+        public string Description { get; set; }
+    }
+}
