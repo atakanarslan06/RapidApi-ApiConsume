808044e4b875beed12c1ee4136a8bf603c84d657
Atakan Arslan <atak.arslan@hotmail.com>
2023-11-09 15:59:46 +0300

    booking

--- ApiConsume/HotelProject.EntityLayer/Concrete/Booking.cs
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Booking.cs
@@ -17,6 +17,7 @@ namespace HotelProject.EntityLayer.Concrete
         public string ChildCount { get; set; }
         public string RoomCount { get; set; }
         public string SpecialRequest { get; set; }
+        public string Description { get; set; }
         public string Status { get; set; }
     }
 }
