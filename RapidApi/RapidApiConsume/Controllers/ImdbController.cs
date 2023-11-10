ccb448cc18b011ee9b60f03a775d88ccbca50d76
Atakan Arslan <atak.arslan@hotmail.com>
2023-11-10 09:21:00 +0300

    RapidApi

---
+++ RapidApi/RapidApiConsume/Controllers/ImdbController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace RapidApiConsume.Controllers
+{
+    public class ImdbController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/top32"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "32d70a1060msh2ed77b94f58b001p1978a6jsn332b7d959551" },
+        { "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+            }
+            return View();
+        }
+    }
+}
