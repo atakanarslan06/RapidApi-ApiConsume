d7ea21777d07efc2c586e7d6cd9588d17e058672
Atakan Arslan <atak.arslan@hotmail.com>
2023-11-13 09:47:53 +0300

    sendmessage

--- ApiConsume/HotelProject.DataAccesLayer/Migrations/20231113064045_mig_add_sendMessage.Designer.cs
+++ ApiConsume/HotelProject.DataAccesLayer/Migrations/20231113064045_mig_add_sendMessage.Designer.cs
@@ -4,6 +4,7 @@ using HotelProject.DataAccesLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelProject.DataAccesLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20231113064045_mig_add_sendMessage")]
+    partial class mig_add_sendMessage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
