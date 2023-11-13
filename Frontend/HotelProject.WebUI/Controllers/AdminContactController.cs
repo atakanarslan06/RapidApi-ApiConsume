a20503a1619ce8e47226d0b8942faa3b38258e1c
Atakan Arslan <atak.arslan@hotmail.com>
2023-11-13 10:18:02 +0300

    sendmessageconsume

--- Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -36,10 +36,13 @@ namespace HotelProject.WebUI.Controllers
         [HttpPost]
         public async Task<IActionResult> AddSendMessage(CreateSendMessage createSendMessage)
         {
+            createSendMessage.SenderMail = "admin@gmail.com";
+            createSendMessage.SenderName = "admin";
+            createSendMessage.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
             var client = _httpClientFactory.CreateClient();
             var jsonData = JsonConvert.SerializeObject(createSendMessage);
             StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-            var responseMessage = await client.PostAsync("http://localhost:5066/api/SendMessage", stringContent);
+            var responseMessage = await client.PostAsync("http://localhost:5066/api/AddSendMessage", stringContent);
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("SendBox");
