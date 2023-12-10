2293c2f3bedede4708ee576e913d42f9f4f6b035
Atakan Arslan <atak.arslan@hotmail.com>
2023-12-10 19:58:38 +0300

    RoleAssign

--- Frontend/HotelProject.WebUI/Controllers/RoleAssignController1.cs
+++ Frontend/HotelProject.WebUI/Controllers/RoleAssignController1.cs
@@ -1,4 +1,5 @@
 ﻿using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Models.Role;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,16 +8,35 @@ namespace HotelProject.WebUI.Controllers
     public class RoleAssignController1 : Controller
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<AppRole> _roleManager;
 
-        public RoleAssignController1(UserManager<AppUser> userManager)
+        public RoleAssignController1(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
         {
             _userManager = userManager;
+            _roleManager = roleManager;
         }
 
         public IActionResult Index()
         {
             var values = _userManager.Users.ToList();
             return View(values);
+        }
+        public async Task<IActionResult> AssignRole(int id)
+        {
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == id);
+            TempData["userid"] = user.Id;
+            var roles = _roleManager.Roles.ToList();
+            var userRoles = await _userManager.GetRolesAsync(user);
+            List<RoleAssignViewModel> roleAssignViewModels = new List<RoleAssignViewModel>();
+            foreach (var item in roles)
+            {
+                RoleAssignViewModel roleAssignViewModel = new RoleAssignViewModel();
+                roleAssignViewModel.RoleID = item.Id;
+                roleAssignViewModel.RoleName = item.Name;
+                roleAssignViewModel.RoleExist = userRoles.Contains(item.Name);
+                roleAssignViewModels.Add(roleAssignViewModel);
+            }
+            return View(roleAssignViewModels);
         }
     }
 }
