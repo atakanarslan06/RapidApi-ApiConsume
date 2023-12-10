7e1641f76515627a7a63e769ca00b9b3af1f3115
Atakan Arslan <atak.arslan@hotmail.com>
2023-12-10 19:39:32 +0300

    RoleAssign

---
+++ Frontend/HotelProject.WebUI/Controllers/RoleAssignController1.cs
@@ -0,0 +1,22 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class RoleAssignController1 : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RoleAssignController1(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _userManager.Users.ToList();
+            return View(values);
+        }
+    }
+}
