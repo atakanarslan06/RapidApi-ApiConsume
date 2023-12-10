a145192319c9ee7a920aeaf7c852f3805ee09ddb
Atakan Arslan <atak.arslan@hotmail.com>
2023-12-10 10:33:55 +0300

    rolecontroller

--- Frontend/HotelProject.WebUI/Controllers/RoleController.cs
+++ Frontend/HotelProject.WebUI/Controllers/RoleController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class RoleController : Controller
     {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var values = _roleManager.Roles.ToList();
+            return View(values);
         }
     }
 }
